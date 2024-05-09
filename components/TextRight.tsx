1115442996c76aa50edc1fcfbf3481888a880053
Gabriel Vincenzi <141547971+GabrielVincenzi@users.noreply.github.com>
2024-05-09 19:12:50 +0200

    Update TextRight.tsx

--- components/TextRight.tsx
+++ components/TextRight.tsx
@@ -12,7 +12,7 @@ const TextRight = ({ title, description, image }: TextProps) => {
             <Image src={image} alt="" width={500} height={500} />
             <div className='text-right relative flex flex-1 flex-col xl:w-1/2'>
                 <h3 className="lg:bold-40">{title}</h3>
-                <p className="description mt-6 pl-40">{description}</p>
+                <p className="description mt-6 pl-5 lg:pl-40">{description}</p>
             </div>
 
         </div>
