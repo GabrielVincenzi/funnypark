322c18f51dbc05f3bfa91335e32fc3cae2c45172
Gabriel Vincenzi <141547971+GabrielVincenzi@users.noreply.github.com>
2024-05-10 09:26:14 +0200

    Update ArticleSubtexts.tsx

--- components/ArticleSubtexts.tsx
+++ components/ArticleSubtexts.tsx
@@ -9,7 +9,7 @@ type TextProps = {
 
 const ArticleSubtexts = ({ title, description, description1, description2, subtitle1, subtitle2 }: TextProps) => {
     return (
-        <div className="lg:px-10 max-container padding-container flex flex-col gap-20 pt-10 pb-10 md:gap-28 lg:pt-20 xl:flex-row">
+        <div className="lg:px-20 px-10 max-container padding-container flex flex-col gap-20 pt-10 pb-10 md:gap-28 lg:pt-20 xl:flex-row">
             <div className='text-left relative flex flex-1 flex-col xl:w-1/2 gap-4'>
                 <h2 className="lg:bold-52">{title}</h2>
                 <p className="description mt-6 lg:pr-40">
